6f186ea2e83c4bf4fd7317787661a400bb129cd3
Bryndye <hugocluzel17@hotmail.fr>
2024-09-16 21:56:11 +0200

    Transition

---
+++ Assets/_scripts/CanvasTransitionManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class CanvasTransitionManager : MonoBehaviour
+{
+    public static CanvasTransitionManager Instance;
+    private GameManager gameManager;
+
+    [SerializeField] private Animator mAnimator;
+
+    [AnimatorParam("mAnimator"), SerializeField]
+    private string enterParam;
+    [AnimatorParam("mAnimator"), SerializeField]
+    private string exitParam;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        mAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+
+        gameManager.OnLoadScene += TransitionOn;
+        gameManager.OnLoadedScene += TransitionOff;
+    }
+
+    public void TransitionOn()
+    {
+        mAnimator.SetTrigger(enterParam);
+    }
+
+    public void TransitionOff()
+    {
+        mAnimator?.SetTrigger(exitParam);
+    }
+
+    //private void Transitions()
+    //{
+    //    TransitionOn();
+    //    TransitionOff();
+    //}
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        Transitions();
+    //    }
+    //}
+}
---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,87 @@
+using NaughtyAttributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using NaughtyAttributes;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LoadLevel(scene_1);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LoadLevel(scene_2);
+        }
+    }
+
+    #region Load Level
+    public Action OnLoadedScene;
+    public Action OnLoadScene;
+    [Scene, SerializeField] private string scene_1;
+    [Scene, SerializeField] private string scene_2;
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        OnLoadScene?.Invoke();
+
+        // Start loading the scene asynchronously
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Prevent the scene from activating until it's fully loaded
+        operation.allowSceneActivation = false;
+
+        // While the scene is loading
+        while (!operation.isDone)
+        {
+            // Get the progress (operation.progress ranges from 0 to 0.9)
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            // Update the UI progress bar (optional)
+            //if (progressBar != null)
+            //{
+            //    progressBar.value = progress;
+            //}
+
+            // Debug log to show progress percentage in the console
+            Debug.Log($"Loading progress: {progress * 100}%");
+
+            // If the loading is complete (progress is at 0.9), activate the scene
+            if (operation.progress >= 0.9f)
+            {
+                // You can add a UI to press a button to activate the scene, or just auto-activate
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        // Scene has fully loaded
+        Debug.Log("Scene loaded completely.");
+        OnLoadedScene?.Invoke();
+    }
+    #endregion
+}
