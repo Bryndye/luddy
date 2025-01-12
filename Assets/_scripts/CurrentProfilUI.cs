cc959ff6d16255f4171d757ac48982773261a2fa
Bryndye <hugocluzel17@hotmail.fr>
2025-01-12 20:24:46 +0100

    Parametre page + profil UI current

---
+++ Assets/_scripts/CurrentProfilUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentProfilUI : MonoBehaviour
+{
+    private AuthManager authManager;
+    [SerializeField] private TextMeshProUGUI profilName;
+    [SerializeField] private Image profilIcon;
+    [SerializeField] private GameObject parameter;
+
+    private void Start()
+    {
+        authManager = AuthManager.Instance;
+
+        SetContent();
+
+        // Forcer la desactivation de la page
+        parameter.SetActive(false);
+    }
+    
+    public void SetContent()
+    {
+        profilName.text = authManager.MyCurrentSubAccount.Nom.ToString();
+    }
+
+
+    public void ActiveParameter()
+    {
+        parameter.SetActive(!parameter.activeSelf);
+    }
+}
